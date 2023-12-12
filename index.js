13ce5772a52ba067e90bbb21d3c789b9975a0444
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-12 20:12:04 +0630

    checkout point is ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -13,36 +13,39 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6iupoas.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, } });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const serviceCollection = client.db('carDoctor').collection('service');
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      console.log(uri);
+      res.send(services);
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    })
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
   }
 }
-run().catch(console.dir);
+run().catch(err => console.error(err));
 
 
-app.get('/', (req, res) =>{
-    res.send('genius car server is running')
+app.get('/', (req, res) => {
+  res.send('genius car server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Genius Car Server Is Running On Port: ${port}`);
+  console.log(`Genius Car Server Is Running On Port: ${port}`);
 })
